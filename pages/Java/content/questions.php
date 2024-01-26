57ad2e6092ce6f59171a894f4823f5a0656b7271
anand <do4connect@gmail.com>
2024-01-26 12:11:15 +0530

    .

---
+++ pages/Java/content/questions.php
@@ -0,0 +1,40 @@
+<div class="topics" id="What_are_various_access_specifiers/_access_modifier_in_Java">
+<!--Heading-->  <h2 class="subheading" >1. What are various access specifiers/ access modifier in Java.</h2>
+
+<p>
+  Access specifier or modifier are used to control the accessability of the class and it's properties and methods.
+</p>
+<p>In java there are four type of access specifier <b>public, private, projected and default</b>.</p>
+<table border="1">
+	<tbody>
+		<tr>
+			<td><strong>Modifier</strong></td>
+			<td><strong>Description</strong></td>
+		</tr>
+		<tr>
+			<td>Default</td>
+			<td>If we do not explicitly specify any access modifier for classes, methods, variables, etc, then by default the default access modifier is considered. declarations are visible only within the package (package private)</td>
+		</tr>
+		<tr>
+			<td>Private</td>
+			<td>declarations are visible within the class only</td>
+		</tr>
+		<tr>
+			<td>Protected</td>
+			<td>declarations are visible within the package or all subclasses</td>
+		</tr>
+		<tr>
+			<td>Public</td>
+			<td>declarations are visible everywhere</td>
+		</tr>
+	</tbody>
+</table>
+
+</div>
+
+<div class="topics" id="What_is_the_purpose_of_static_method_and_variable">
+  <h2  class="subheading" >2. What is the purpose of static method and variable.</h2>
+  <p>
+  Whenever a variable is declared as static, this means there is only one copy of it for the entire class, rather than each object having its own copy. A static method means it can be called without creating an object of the class.
+  </p>
+</div>
---
+++ pages/Java/content/tableofcontent.php
@@ -0,0 +1,2 @@
+<li onclick="openNavigation()"><a href="#What_are_various_access_specifiers/_access_modifier_in_Java">What are various access specifiers/ access modifier in Java.</a></li>
+<li onclick="openNavigation()"><a href="#What_is_the_purpose_of_static_method_and_variable">What is the purpose of static method and variable.</a></li>
